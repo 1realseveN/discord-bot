2c09d9310c5185441a2842b8919f8e9b6b142fc5
1realseveN <115028735+1realseveN@users.noreply.github.com>
2025-05-07 20:37:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 TOKEN = os.getenv("DISCORD_TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 IMAGE_URL = os.getenv("IMAGE_URL")
+IMAGE_URL_2 = os.getenv("IMAGE_URL_2")
 
 # === Scheduler: jeden Freitag 12:00 Uhr (Berlin-Zeit)
 scheduler = AsyncIOScheduler()
@@ -52,18 +53,35 @@ async def on_ready():
                       hour=12,
                       minute=0,
                       timezone='Europe/Berlin')
+    scheduler.add_job(send_monday_image,
+                      'cron',
+                      day_of_week='mon',
+                      hour=12,
+                      minute=0,
+                      timezone='Europe/Berlin')
     scheduler.start()
 
 
 @bot.command()
-async def rochenende(ctx):
+async def freitag(ctx):
     await ctx.send(IMAGE_URL)
+
+@bot.command()
+async def montag(ctx):
+    await ctx.send(IMAGE_URL_2)
 
 
 async def send_friday_image():
     channel = bot.get_channel(CHANNEL_ID)
     if channel:
         await channel.send(IMAGE_URL)
+    else:
+        print("⚠️ Channel nicht gefunden.")
+
+async def send_monday_image():
+    channel = bot.get_channel(CHANNEL_ID)
+    if channel:
+        await channel.send(IMAGE_URL_2)
     else:
         print("⚠️ Channel nicht gefunden.")
 
